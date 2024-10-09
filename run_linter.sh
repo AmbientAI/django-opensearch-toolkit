0a526f40f0a1c79cb55eb760e723861c2a70732e
David Tagliamonti <32236720+davidtag@users.noreply.github.com>
2024-10-09 16:51:21 -0700

    Add type checks to linter check (#5)
    * Add type checks to linter check
    
    * fix type errors
    
    * Add base requirements for linter workflow

--- run_linter.sh
+++ run_linter.sh
@@ -2,6 +2,8 @@ set -e
 
 flake8 django_opensearch_toolkit
 pylint django_opensearch_toolkit
+mypy django_opensearch_toolkit
 
 flake8 sample_project
 pylint sample_project
+mypy sample_project
--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-%u*m&-l6-5a@nsqfu*7ioia7x26==k!d%4e@ye(xa*)m*x=%b5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
