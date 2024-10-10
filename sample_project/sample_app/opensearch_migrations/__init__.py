cae8d4bfcefa026cbbf37747bedd2a0c62e2e17b
David Tagliamonti <dtag@ambient.ai>
2024-10-09 17:36:23 -0700

    Add some sample migrations

--- sample_project/sample_app/opensearch_migrations/__init__.py
+++ sample_project/sample_app/opensearch_migrations/__init__.py
@@ -4,6 +4,9 @@ from typing import List
 
 from django_opensearch_toolkit.migration_manager import OpenSearchMigration
 
+from .m0001_migration_one import SampleMigration0001
+from .m0002_migration_two import SampleMigration0002
+
 
 # When making updates here, please note these guidelines:
 #   - Don't remove an entry from this list
@@ -11,4 +14,7 @@ from django_opensearch_toolkit.migration_manager import OpenSearchMigration
 #   - Only add new entries at the end
 # We use a serial migration order right now for OpenSearch. It's a dependency chain,
 # instead of a more general dependency graph, like Django would create.
-MIGRATIONS: List[OpenSearchMigration] = []
+MIGRATIONS: List[OpenSearchMigration] = [
+    SampleMigration0001(),
+    SampleMigration0002(),
+]
---
+++ sample_project/sample_app/opensearch_migrations/m0001_migration_one.py
@@ -0,0 +1,26 @@
+"""Sample migration #1."""
+
+from opensearch_dsl.connections import connections
+
+from django_opensearch_toolkit.migration_manager import OpenSearchMigration
+
+
+class SampleMigration0001(OpenSearchMigration):
+    """Sample migration #1."""
+
+    _KEY = "0001_migration_one"
+    _DESCRIPTION = "Sample migration #1"
+
+    def __init__(self) -> None:
+        """Initialize the migration."""
+        super().__init__(key=self._KEY)
+
+    def serialize(self) -> str:
+        """Return a textual description of the migration run to store in the log."""
+        return self._DESCRIPTION
+
+    def apply(self, connection_name: str) -> bool:
+        """Perform the migration."""
+        client = connections.get_connection(connection_name)
+        del client  # Use the low-levelclient to execute commands against the cluster
+        return True  # return True on success, False on failure
