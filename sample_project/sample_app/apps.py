c88c6e592385676aa11973f870e163e8f8e8304b
David Tagliamonti <dtag@ambient.ai>
2024-10-09 17:26:20 -0700

    fix linter

--- sample_project/sample_app/apps.py
+++ sample_project/sample_app/apps.py
@@ -1,6 +1,10 @@
+"""Configuration for the sample_app app."""
+
 from django.apps import AppConfig
 
 
 class SampleAppConfig(AppConfig):
+    """Configuration for the sample_app app."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "sample_app"
