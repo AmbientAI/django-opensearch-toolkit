f306bd2c71b5562bf0868d188067ba50f9e7c5eb
David Tagliamonti <dtag@ambient.ai>
2025-02-09 11:04:04 -0800

    Fix typo

--- django_opensearch_toolkit/migration_manager/__init__.py
+++ django_opensearch_toolkit/migration_manager/__init__.py
@@ -5,7 +5,7 @@ cluster outside the data itself. This could be cluster settings, creation of
 specific indices or lifecycle policies on them, changes to mappings, etc.
 
 These are analagous to Django migrations and this package aims to provide a
-lightweight version of what Django migrations provides for RDBMS systems.
+lightweight version of what Django migrations provides for RDBMS databases.
 """
 
 from .opensearch_migration import OpenSearchMigration
